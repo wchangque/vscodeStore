0acc44f8763e4efb5ed2e36f8330518781b1e087
wchangque <wuzhen1225@qq.com>
2022-09-09 11:08:16 +0800

    建立仓库，本仓库用于数据结构和算法学习

---
+++ build/CMakeFiles/firstProject.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/firstProject.dir/main.cpp.obj"
+  "CMakeFiles/firstProject.dir/main.cpp.obj.d"
+  "firstProject.exe"
+  "firstProject.exe.manifest"
+  "firstProject.pdb"
+  "libfirstProject.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/firstProject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/firstProject.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for firstProject.
+# This may be replaced when dependencies are built.
